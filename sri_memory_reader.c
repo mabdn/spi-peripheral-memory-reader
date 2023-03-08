6ad8409f73c3a9e2f5316929f0497e262e36e91e
Marius Baden <marius.baden@uga.edu>
2023-03-08 14:52:06 -0500

    Extend prototype to two packets in a row

--- sri_memory_reader.c
+++ sri_memory_reader.c
@@ -2,25 +2,35 @@
 #include "component.h"
 #define BYTE_SIZE 8
 
-
-int main() {
+int main()
+{
     printf("Hello World !\n");
+    int a = sizeof(long);
     SET_CLK(true);
     unsigned char read_command = 0x55;
     unsigned char address = 0x31;
     unsigned char tail = 0x00;
-    int packet = tail << 2*BYTE_SIZE | address << BYTE_SIZE | read_command;
+    long packet = tail << 2 * BYTE_SIZE | address << BYTE_SIZE | read_command;
+    packet = 0x003155003155;
+    long sent_mosi = 0;
+    long received_miso = 0;
 
-    for (int i = 0; i < 3*BYTE_SIZE; i++) {
+    for (int i = 0; i < 8 * BYTE_SIZE; i++)
+    {
         printf("--- Cycle %d ---\n", i);
         SET_CLK(true);
 
-        int mosi_bit = (packet >> i) & 1;
+        long mosi_bit = (packet >> i) & 1;
         printf("mosi_bit: %d\n", mosi_bit);
         SET_MOSI(mosi_bit);
+        sent_mosi = sent_mosi | (mosi_bit << i);
 
-        int miso_bit = READ_MISO();
+        long miso_bit = READ_MISO();
         printf("miso_bit: %d\n", miso_bit);
+        received_miso = received_miso | (miso_bit << i);
+
         SET_CLK(false);
-    } 
+    }
+    printf("sent_mosi    : %016lx\n", sent_mosi);
+    printf("received_miso: %016lx\n", received_miso);
 }
